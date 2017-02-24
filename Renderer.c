55e787b77d444f657883aabb03eaf686ce964e2c
N00byEdge <me@hannesbredberg.se>
2017-02-24 21:01:45 +0100

    Renderer fix

--- Renderer.c
+++ Renderer.c
@@ -1,6 +1,6 @@
 #include "Renderer.h"
 
-void renderTile(const unsigned *renderThis, unsigned *ontoThis, int startX, int startY, int endX, int endY, int atX, int atY) {
+void renderTile(const unsigned char *renderThis, unsigned char *ontoThis, int startX, int startY, int endX, int endY, int atX, int atY) {
 	for(int x = startX; x < endX; ++ x) {
 		for(int y = startY; y < endY; ++ y) {
 			if(renderThis[3 + x*4 + y*4*16]) {
@@ -8,6 +8,17 @@ void renderTile(const unsigned *renderThis, unsigned *ontoThis, int startX, int 
 					ontoThis[c + x*4 + y*4*16 + atX*4 + atY*4*16] = (float) (255-renderThis[3 + x*4 + y*4*16])/255 * ontoThis[c + x*4 + y*4*16 + atX*4 + atY*4*16]
 											 + (float) (renderThis[3 + x*4 + y*4*16])/255 * renderThis[c + x*4 + y*4*16];
 				ontoThis[3 + x*4 + y*4*16 + atX*4 + atY*4*16] += (float) (255 - ontoThis[3 + x*4 + y*4*16 + atX*4 + atY*4*16])/255 * renderThis[3 + x*4 + y*4*16];
+			}
+		}
+	}
+}
+
+void renderSimple(const unsigned char *renderThis, unsigned char *ontoThis, int startX, int startY, int endX, int endY, int atX, int atY) {
+	for(int x = startX; x < endX; ++ x) {
+		for(int y = startY; y < endY; ++ y) {
+			if(renderThis[3 + x*4 + y*4*16]) {
+				for(int c = 0; c < 3; ++ c)
+					ontoThis[c + x*4 + y*4*16] = renderThis[c + x*4 + y*4*16];
 			}
 		}
 	}
--- Renderer.h
+++ Renderer.h
@@ -3,4 +3,5 @@
 #include "Parameters.h"
 #include "Board.h"
 
-void renderTile(const unsigned *renderThis, unsigned *ontoThis, int startX, int startY, int endX, int endY, int atX, int atY);
+void renderTile(const unsigned char *renderThis, unsigned char *ontoThis, int startX, int startY, int endX, int endY, int atX, int atY);
+void renderSimple(const unsigned char *renderThis, unsigned char *ontoThis, int startX, int startY, int endX, int endY, int atX, int atY);
