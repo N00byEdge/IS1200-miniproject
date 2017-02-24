b8cf42fea6d997587bb3f4df70f3301dacc62cfd
N00byEdge <me@hannesbredberg.se>
2017-02-24 23:31:54 +0100

    Board drawing

--- Board.c
+++ Board.c
@@ -1,5 +1,11 @@
 #include "Board.h"
 
+#include "ImageData.h"
+
+unsigned char backgroundColor[4] = {0xaa, 0xaa, 0xaa, 0xff};
+unsigned char redColor[4] = {0xff, 0x00, 0x00, 55};
+unsigned char greenColor[4] = {0x00, 0xff, 0x00, 55};
+
 void init(enum tileType *b) {
 	for(int col = 0; col < COLUMNS; ++ col)
 		for(int row = 0; row < ROWS; ++ row)
@@ -7,13 +13,33 @@ void init(enum tileType *b) {
 }
 
 void printBoard(enum tileType *b) {
-	setArea(XRES - TILE_WIDTH /2 * COLUMNS,
-			XRES + TILE_WIDTH /2 * COLUMNS,
-			YRES - TILE_HEIGHT/2 * ROWS,
-			YRES + TILE_HEIGHT/2 * ROWS);
+	int boardMinXPixel = XRES/2 - TILE_WIDTH /2 * COLUMNS;
+	int boardMinYPixel = YRES/2 - TILE_HEIGHT/2 * ROWS;
+
+	paint(colorsTo16Bit(backgroundColor));
+
+	for(int x = 0; x < COLUMNS; ++ x)
+		for(int y = 0; y < ROWS; ++ y)
+			b[x + y*COLUMNS] |= (x+y)%2 ? ((x+y)%3 ? TILE_SHIP : TILE_MISS) : ((x+y)%3 ? 0 : TILE_HIT);
+
 	for(int y = 0; y < ROWS; ++ y) {
 		for(int x = 0; x < COLUMNS; ++ x) {
-
+			unsigned char drawArea[16*16*4];
+			fillColor(drawArea, backgroundColor, 16, 16);
+			if((b[x + y*COLUMNS] & TILE_IS_PLACING) &&
+			   (b[x + y*COLUMNS] & TILE_SHIP))
+				fillColor(drawArea, redColor, 16, 16);
+			else if(b[x + y*COLUMNS] & TILE_IS_PLACING) {
+				fillColor(drawArea, greenColor, 16, 16);
+			}
+			frame(drawArea);
+			if(b[x + y*COLUMNS] & TILE_MISS)
+				renderTile(missPicture, drawArea, 0, 0, 16, 16, 0, 0);
+			if(b[x + y*COLUMNS] & TILE_HIT)
+				renderTile(explodePicture, drawArea, 0, 0, 16, 16, 0, 0);
+			if(b[x + y*COLUMNS] & TILE_SHIP)
+				renderTile(shipPicture, drawArea, 0, 0, 16, 16, 0, 0);
+			paintimg(drawArea, 16, 16, boardMinXPixel + x*16, boardMinYPixel + y*16);
 		}
 	}
 }
--- Board.h
+++ Board.h
@@ -2,12 +2,15 @@
 
 #include "Parameters.h"
 #include "Display.h"
+#include "Renderer.h"
 
 enum tileType {
 	TILE_EMPTY = 1,
 	TILE_HIT = 1 << 1,
 	TILE_MISS = 1 << 2,
 	TILE_SHIP = 1 << 3,
+	TILE_IS_PLACING = 1 << 4,
+	TILE_IS_AIMING = 1 << 4
 };
 
 void init(enum tileType *b);
