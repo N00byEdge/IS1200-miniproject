ed363678e86d46598fe292b4be894018e86736a1
N00byEdge <me@hannesbredberg.se>
2017-02-25 00:54:33 +0100

    Input!

--- Input.c
+++ Input.c
@@ -4,11 +4,17 @@
 // TODO: find pins for buttons
 
 void buttonsInit(){
-
+	TRISDSET = 0x7 | (0x3 << 9); //0, 1, 2, 9, 10
+	TRISFSET = (1 << 1); // 1
 }
 
 unsigned getButtons() {
-	return 0x7 &(PORTB >> 5);
+	return ((PORTD >> 0) & 0x1) |
+		   ((PORTF >> 1) & 0x1) << 1 |
+		   ((PORTD >> 1) & 0x1) << 2 |
+		   ((PORTD >> 2) & 0x1) << 3 |
+		   ((PORTD >> 9) & 0x1) << 4 |
+		   ((PORTD >> 10) & 0x1) << 5;
 }
 
 unsigned getButtonAccept() {
