1e929d9d131eba7791d9c4ed647d9a3087c14d77
N00byEdge <me@hannesbredberg.se>
2017-02-27 15:01:57 +0100

    In the future, I will make sure these compile too.

--- Minicomms.c
+++ Minicomms.c
@@ -18,18 +18,18 @@ struct packet listenShot(enum tileType *board) {
 	static int x = 5;
 	static int y = 6;
 	struct packet p;
-	p->x = ++x%10;
-	p->y = ++y%10;
+	p.x = ++x%10;
+	p.y = ++y%10;
 
 	// Recieve data
 
-	if(board[p->x + p->y*COLUMNS] |= TILE_SHIP)
-		p->didHit = 1;
+	if(board[p.x + p.y*COLUMNS] |= TILE_SHIP)
+		p.didHit = 1;
 
-	p->didWin = 1;
+	p.didWin = 1;
 	for(int i = 0; i < ROWS*COLUMNS; ++ i)
 		if((board[i] & TILE_SHIP) && ((board[i]) & TILE_HIT) == 0)
-			p->didWin = 0;
+			p.didWin = 0;
 
 	// Send back didWin and didHit data
 
